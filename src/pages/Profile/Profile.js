793fbd2bbdb53cf4a871fde5315f0ce88989c591
Flávio Eduardo <xflax00@gmail.com>
2023-03-06 19:05:39 -0300

    profile.js

--- src/pages/Profile/Profile.js
+++ src/pages/Profile/Profile.js
@@ -6,22 +6,21 @@ import { AuthContext } from "../../context/AuthContext"
 
 const Profile = () => {
 
-    const { logOut } = useAuth()
-    const { auth } = useContext(AuthContext)
+  const { logOut } = useAuth()
+  const { auth } = useContext(AuthContext)
 
-    const handleClick = () => {
-        logOut()
-    }
+  const handleClick = () => {
+      logOut()
+  }
 
   return (
-
     <div className="Profile">
         <h1>Profile</h1>
         <img src={auth.currentUser.photoURL} alt="perfil" />
         <p><span>Nome:</span> {auth.currentUser.displayName}</p>
         <p><span>Email:</span> {auth.currentUser.email}</p>
 
-        <button onClick={handleClick}>Sair</button>
+        <button onClick={handleClick}>Sair</button>        
     </div>
   )
 }
